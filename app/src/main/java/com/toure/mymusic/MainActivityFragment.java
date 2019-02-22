fa8a78cbcb325f6d94adfff7ec7b984ae1a74c44
TOURE NATHAN <toure21@live.com>
2019-02-22 13:46:02 +0100

    Created the Project and did the basic setup and configurations

---
+++ app/src/main/java/com/toure/mymusic/MainActivityFragment.java
@@ -0,0 +1,34 @@
+package com.toure.mymusic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+public class MainActivityFragment extends Fragment {
+
+    private MainActivityViewModel mViewModel;
+
+    public static MainActivityFragment newInstance() {
+        return new MainActivityFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_activity_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/toure/mymusic/MainActivityViewModel.java
@@ -0,0 +1,7 @@
+package com.toure.mymusic;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
