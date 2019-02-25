89c012d8efa1ac7655046a1ae787036cb0efaacb
TOURE NATHAN <toure21@live.com>
2019-02-25 17:44:27 +0100

    Created the DetailActivity which will be used to display the Details of an Album

---
+++ app/src/main/java/com/toure/mymusic/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.toure.mymusic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
